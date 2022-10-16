9d5a9f414fadda515ffa3a21c4693d31afbf9486
Vadim <crocozybra.v@gmail.com>
2022-10-16 23:30:32 +0300

    Refactoring

--- js/renderElements.js
+++ js/renderElements.js
@@ -2,11 +2,12 @@ const pictureElement = document.body.querySelector('.pictures');
 let fragment = document.createDocumentFragment();
 
 const createElement = (bildingElement, template) => {
-  template.querySelector('.picture').setAttribute('href', bildingElement.url);
-  template.querySelector('.picture').setAttribute('data-id', bildingElement.id);
-  template.querySelector('.picture__img').setAttribute('src', bildingElement.url);
+  const {url, id, likes} = bildingElement;
+  template.querySelector('.picture').setAttribute('href', url);
+  template.querySelector('.picture').setAttribute('data-id', id);
+  template.querySelector('.picture__img').setAttribute('src', url);
   template.querySelector('.picture__comments').textContent = bildingElement.comments.length;
-  template.querySelector('.picture__likes').textContent = bildingElement.likes;
+  template.querySelector('.picture__likes').textContent = likes;
   return template;
 };
 
